b14165423da74bd867501f3a8a5deb4c290af782
rajib5570 <rajib5570@gmail.com>
2024-06-03 00:50:28 +0600

    navbar, dashboard, contact-us page added

--- src/layouts/Mainlayout.jsx
+++ src/layouts/Mainlayout.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../components/shared/Navbar';
 
 const Mainlayout = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Mainlayout from "../layouts/Mainlayout";
 import Home from "../pages/home/layout/Home";
+import Dashboard from "../pages/dashboard/layout/Dashboard";
+import ContactUs from "../pages/contactUs/layout/ContactUs";
 
 export const router = createBrowserRouter([
     {
@@ -11,6 +13,14 @@ export const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/dashboard',
+                element : <Dashboard></Dashboard>
+            },
+            {
+                path: '/contact-us',
+                element: <ContactUs></ContactUs>
             }
         ]
     }
