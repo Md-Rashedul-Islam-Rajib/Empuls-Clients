ad076790b5b3a57b68d85e54175319b1fe96bda5
rajib5570 <rajib5570@gmail.com>
2024-06-12 04:29:11 +0600

    contact us page modified

--- src/pages/contactUs/layout/ContactUs.jsx
+++ src/pages/contactUs/layout/ContactUs.jsx
@@ -1,22 +1,113 @@
-import React from 'react';
+import { useForm } from "react-hook-form";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { IoMdTime } from "react-icons/io";
+import { IoLocationSharp } from "react-icons/io5";
+import { axiosPublic } from "../../../hooks/useAxiosPublic";
+import Swal from "sweetalert2";
 
 const ContactUs = () => {
-    return (
-        <div>
-           <div className='text-center'>
-            <h2>We’d love to hear from you</h2>
-            <p>Whether you’re curious about anything about us,we're ready to answer any and all questions.</p>
-           </div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset
+  } = useForm();
 
-           <div>
+  const onSubmit = async (data) => {
+   
+      axiosPublic.post("/messages",data)
+      .then((res)=> {
+          if (res.data.insertedId){
+            reset()
+              Swal.fire({
+                  position: "center",
+                  icon: "success",
+                  title: "Your message has been send",
+                  showConfirmButton: false,
+                  timer: 1500
+                });
+          }
+      })
+    };
 
-            <div></div>
-            <div></div>
+  return (
+    <div
+      className="h-screen"
+      style={{ backgroundImage: "url(/Contactus.jpg)" }}
+    >
+      <div className="text-white flex flex-col lg:flex-row size-full">
+        <div className="flex flex-col gap-10 justify-center lg: ml-60">
+          <h2 className="text-4xl font-bold">Contact Us</h2>
 
+          <div>
+            <div className="flex items-center gap-2 text-xl font-medium my-2">
+              <span>
+                <BsFillTelephoneFill />
+              </span>{" "}
+              Call Us{" "}
+            </div>
+            <p>1 (234) 567-891, 1 (234) 987 654</p>
+          </div>
 
-           </div>
+          <div>
+            <div className="flex items-center gap-2 text-xl font-medium my-2">
+              <span>
+                <IoLocationSharp />
+              </span>{" "}
+              Location
+            </div>
+            <p>121 Rock Street, 21 Avenue, New York, NY 92103</p>
+          </div>
+
+          <div>
+            <div className="flex items-center gap-2 text-xl font-medium my-2">
+              <span>
+                <IoMdTime />
+              </span>{" "}
+              Business Hours{" "}
+            </div>
+            <p>Mon-Fri.....10am-8pm, sat,sun.....closed</p>
+          </div>
         </div>
-    );
+
+        <div className="flex-1 flex items-center justify-center">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-4 mb-4">
+                <p>Email</p>
+                <label className="input input-bordered flex items-center gap-2 ">
+              <input
+                type="text"
+                className="grow"
+                placeholder="Enter Your Email"
+                {...register("email", { required: true })}
+              />
+            </label>
+            </div>
+            {errors.email && (
+              <p className="text-red-600 text-sm">Email is required</p>
+            )}
+            
+              <div className="flex flex-col gap-3 mb-4">
+                <p>Message</p>
+                <textarea
+                type="text"
+                className="textarea text-black"
+                placeholder="Enter Your Message"
+                {...register("message", { required: true })}
+              />
+              </div>
+            
+            {errors.message && (
+              <p className="text-red-600 text-sm">Message is required</p>
+            )}
+                <div>
+                <button className="btn bg-[#6F42C1] text-white w-full border-0">Submit</button>
+                </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ContactUs;
