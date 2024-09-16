fc00dd4546a4cec5130cd4003c6bb8dba204a514
Md Rashedul Islam <rajib5570@gmail.com>
2024-09-16 23:33:54 +0600

    navbar modified

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,27 +1,26 @@
 import { Link, NavLink } from 'react-router-dom';
-import logo from '../../assets/logo.png'
+import logo from '../../assets/logo.png';
 import { AuthContext } from '../../context/AuthProvider';
 import { useContext, useEffect, useRef, useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
-import userimage from "../../assets/user.png"
+import userimage from "../../assets/user.png";
 import { useQuery } from '@tanstack/react-query';
 import { axiosPublic } from '../../hooks/useAxiosPublic';
 
 const Navbar = () => {
-  const {user, logOutUser} = useContext(AuthContext);
+  const { user, logOutUser } = useContext(AuthContext);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [isDropdownMobileOpen, setIsDropdownMobileOpen] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false); // State for mobile menu
   const dropdownRef = useRef(null);
-  const dropdownMobileRef = useRef(null);
 
   const { data: userInfo = [] } = useQuery({
     queryKey: ['navbar', user?.email],
     queryFn: async () => {
       const response = await axiosPublic.get('/users', {
-        params: { email: user?.email }
+        params: { email: user?.email },
       });
       return response.data;
-    }
+    },
   });
 
   const handleSignOut = () => {
@@ -36,84 +35,134 @@ const Navbar = () => {
         setIsDropdownOpen(false); // Close the dropdown if click outside
       }
     }
-    
-    document.addEventListener("mousedown", handleClickOutside);
+
+    document.addEventListener('mousedown', handleClickOutside);
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener('mousedown', handleClickOutside);
     };
   }, [dropdownRef]);
 
-  const navs = <>
-    <NavLink to='/'
-      className={({ isActive }) =>
-        `block py-1 px-2 ${isActive ? 'bg-[#6F42C1] text-white' : 'text-black'} text-center font-medium rounded-lg`
-      }
-    > <li>Home</li> </NavLink>
-    <NavLink to='/dashboard'
-      className={({ isActive }) =>
-        `block py-1 px-2 ${isActive ? 'bg-[#6F42C1] text-white' : 'text-black'} text-center font-medium rounded-lg`
-      }
-    > <li>Dashboard</li> </NavLink>
-    <NavLink to='/contact-us'
-      className={({ isActive }) =>
-        `block py-1 px-2 ${isActive ? 'bg-[#6F42C1] text-white' : 'text-black'} text-center font-medium rounded-lg`
-      }
-    > <li>Contact Us</li> </NavLink>
-    {!user && <NavLink to='register'
-      className={({ isActive }) =>
-        `block py-1 px-2 ${isActive ? 'bg-[#6F42C1] text-white' : 'text-black'} text-center font-medium rounded-lg`
-      }
-    > <li>Register</li> </NavLink>}
-  </>;
+  const navs = (
+    <>
+      <NavLink
+        to='/'
+        className={({ isActive }) =>
+          `block py-1 px-2 ${
+            isActive ? 'bg-[#6F42C1] text-white' : 'text-black'
+          } text-center font-medium rounded-lg`
+        }
+      >
+        <li>Home</li>
+      </NavLink>
+      <NavLink
+        to='/dashboard'
+        className={({ isActive }) =>
+          `block py-1 px-2 ${
+            isActive ? 'bg-[#6F42C1] text-white' : 'text-black'
+          } text-center font-medium rounded-lg`
+        }
+      >
+        <li>Dashboard</li>
+      </NavLink>
+      <NavLink
+        to='/contact-us'
+        className={({ isActive }) =>
+          `block py-1 px-2 ${
+            isActive ? 'bg-[#6F42C1] text-white' : 'text-black'
+          } text-center font-medium rounded-lg`
+        }
+      >
+        <li>Contact Us</li>
+      </NavLink>
+      {!user && (
+        <NavLink
+          to='register'
+          className={({ isActive }) =>
+            `block py-1 px-2 ${
+              isActive ? 'bg-[#6F42C1] text-white' : 'text-black'
+            } text-center font-medium rounded-lg`
+          }
+        >
+          <li>Register</li>
+        </NavLink>
+      )}
+    </>
+  );
 
   return (
     <nav>
-      <div className="navbar bg-base-100 pr-8">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-            </div>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 z-10 rounded-box w-52">
-              {navs}
-            </ul>
+      <div className='navbar bg-base-100 pr-8'>
+        <div className='navbar-start'>
+          {/* Toggle Button for Mobile */}
+          <div className='dropdown'>
+            <button
+              tabIndex={0}
+              role='button'
+              className='btn btn-ghost lg:hidden'
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} // Toggle state
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-5 w-5'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M4 6h16M4 12h8m-8 6h16'
+                />
+              </svg>
+            </button>
+            {/* Conditional Rendering for Mobile Menu */}
+            {isMobileMenuOpen && (
+              <ul className='menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 z-10 rounded-box w-52'>
+                {navs}
+              </ul>
+            )}
           </div>
-          <a className="btn btn-ghost text-xl">
+
+          {/* Logo */}
+          <a className='btn btn-ghost text-xl'>
             <Link to='/'>
-              <img src={logo} className='h-10' alt="" />
+              <img src={logo} className='h-10' alt='' />
             </Link>
           </a>
         </div>
 
-        <div className="navbar-end">
-          <ul className="menu menu-horizontal gap-4 px-1 mr-4 hidden lg:flex">
+        <div className='navbar-end'>
+          {/* Navigation Links for Desktop */}
+          <ul className='menu menu-horizontal gap-4 px-1 mr-4 hidden lg:flex'>
             {navs}
           </ul>
 
+          {/* User Dropdown */}
           {user ? (
-            <div className="relative" ref={dropdownRef}>
+            <div className='relative' ref={dropdownRef}>
               <div
                 tabIndex={0}
-                role="button"
-                className="btn btn-ghost btn-circle avatar"
+                role='button'
+                className='btn btn-ghost btn-circle avatar'
                 onClick={() => setIsDropdownOpen(!isDropdownOpen)}
               >
                 {userInfo ? (
-                  <img className="rounded-full" src={userInfo?.image} alt="User" />
+                  <img className='rounded-full' src={userInfo?.image} alt='User' />
                 ) : (
-                  <img className="rounded-full" src={userimage} alt="Default" />
+                  <img className='rounded-full' src={userimage} alt='Default' />
                 )}
               </div>
 
               {isDropdownOpen && (
                 <ul
                   tabIndex={0}
-                  className="absolute left-0 mt-3 z-[10] p-2 shadow bg-base-100 rounded-box w-24"
+                  className='absolute left-0 mt-3 z-[10] p-2 shadow bg-base-100 rounded-box w-24'
                   style={{ top: '100%', transform: 'translateX(-20%)' }} // Adjust this value as needed
                 >
                   <button
                     onClick={handleSignOut}
-                    className="btn btn-sm bg-[#6f42c1] text-sm text-white"
+                    className='btn btn-sm bg-[#6f42c1] text-sm text-white'
                   >
                     Log Out
                   </button>
@@ -121,8 +170,8 @@ const Navbar = () => {
               )}
             </div>
           ) : (
-            <Link to="/login">
-              <button className="btn bg-[#6F42C1] text-white">Login</button>
+            <Link to='/login'>
+              <button className='btn bg-[#6F42C1] text-white'>Login</button>
             </Link>
           )}
         </div>
--- src/pages/dashboard/employee-list/layout/EmployeeList.jsx
+++ src/pages/dashboard/employee-list/layout/EmployeeList.jsx
@@ -1,4 +1,4 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../../../context/AuthProvider";
 import useAxiosPublic from "../../../../hooks/useAxiosPublic";
@@ -16,6 +16,7 @@ const EmployeeList = () => {
   const axiosPublic = useAxiosPublic();
   const [selectedUserid, setSelectedUserid] = useState(null);
   const [paymentinfo, setPaymentinfo] = useState(null);
+  const [isTableView, setIsTableView] = useState(true); // Toggle state for desktop
 
   const {
     register,
@@ -50,165 +51,225 @@ const EmployeeList = () => {
 
   return (
     <div className="p-4 md:p-8">
-      <div className="overflow-x-auto">
-        <table className="table table-xs w-full">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Verify Status</th>
-              <th>Bank A/C no</th>
-              <th>Salary</th>
-              <th></th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {userInfo?.map((item, idx) => (
-              <tr key={idx} className="hover">
-                <th>{item?.name ? item?.name : "N/A"}</th>
-                <td>{item?.email ? item?.email : "N/A"}</td>
-                <td>
-                  {item.isVerified ? (
-                    <BsCheckSquareFill className="text-green-500" />
-                  ) : (
-                    <ImCross
-                      onClick={() => {
-                        handleVerification(item?._id);
-                      }}
-                      className="text-red-600 cursor-pointer"
-                    />
-                  )}
-                </td>
-                <td>{item?.bank_account_no ? item?.bank_account_no : "N/A"}</td>
-                <td>{item?.salary ? item?.salary : "N/A"}</td>
-                <td>
-                  <span className="px-3 py-1 font-semibold rounded-md bg-violet-400 text-gray-900">
-                    {item?.isVerified ? (
-                      <>
-                        <button
-                          onClick={() => {
-                            setSelectedUserid(item._id);
-                            document.getElementById("item?._id").showModal();
-                          }}
-                          className="cursor-pointer"
-                        >
-                          Pay
-                        </button>
-                        {/* Modal */}
-                        <dialog
-                          id={"item?._id"}
-                          className="modal modal-bottom sm:modal-middle"
-                        >
-                          <div className="modal-box">
-                            <form onSubmit={handleSubmit(onSubmit)}>
-                              <div className="">
-                                <label className="input flex items-center">
-                                  Email:
-                                  <input
-                                    value={item.email}
-                                    type="email"
-                                    className="grow"
-                                    {...register("email", {
-                                      required: true,
-                                    })}
-                                  />
-                                </label>
-                              </div>
-                              <div className="flex flex-col md:flex-row gap-4">
-                                <div className="w-full md:w-1/3">
+      {/* Toggle Button for Desktop */}
+      <div className="flex justify-end mb-4">
+        <button
+          onClick={() => setIsTableView(!isTableView)}
+          className="btn btn-sm bg-[#6F42C1] text-white hidden lg:block"
+        >
+          {isTableView ? "Switch to Grid View" : "Switch to Table View"}
+        </button>
+      </div>
+
+      {/* Table View (Only for Desktop) */}
+      {isTableView && (
+        <div className="overflow-x-auto hidden lg:block">
+          <table className="table table-lg w-full">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Verify Status</th>
+                <th>Bank A/C no</th>
+                <th>Salary</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {userInfo?.map((item, idx) => (
+                <tr key={idx} className="hover">
+                  <th>{item?.name ? item?.name : "N/A"}</th>
+                  <td>{item?.email ? item?.email : "N/A"}</td>
+                  <td>
+                    {item.isVerified ? (
+                      <BsCheckSquareFill className="text-green-500" />
+                    ) : (
+                      <ImCross
+                        onClick={() => {
+                          handleVerification(item?._id);
+                        }}
+                        className="text-red-600 cursor-pointer"
+                      />
+                    )}
+                  </td>
+                  <td>{item?.bank_account_no ? item?.bank_account_no : "N/A"}</td>
+                  <td>{item?.salary ? item?.salary : "N/A"}</td>
+                  <td>
+                    <span className="px-3 py-1 font-semibold rounded-md bg-violet-400 text-gray-900">
+                      {item?.isVerified ? (
+                        <>
+                          <button
+                            onClick={() => {
+                              setSelectedUserid(item._id);
+                              document.getElementById("item?._id").showModal();
+                            }}
+                            className="cursor-pointer"
+                          >
+                            Pay
+                          </button>
+                          {/* Modal */}
+                          <dialog
+                            id={"item?._id"}
+                            className="modal modal-bottom sm:modal-middle"
+                          >
+                            <div className="modal-box">
+                              <form onSubmit={handleSubmit(onSubmit)}>
+                                <div className="">
                                   <label className="input flex items-center">
-                                    Salary:
+                                    Email:
                                     <input
-                                      value={item.salary}
-                                      type="number"
+                                      value={item.email}
+                                      type="email"
                                       className="grow"
-                                      {...register("salary", {
+                                      {...register("email", {
                                         required: true,
                                       })}
                                     />
                                   </label>
-                                  {errors.salary && (
-                                    <span className="text-red-600 text-sm">
-                                      Salary is required
-                                    </span>
-                                  )}
                                 </div>
+                                <div className="flex flex-col md:flex-row gap-4">
+                                  <div className="w-full md:w-1/3">
+                                    <label className="input flex items-center">
+                                      Salary:
+                                      <input
+                                        value={item.salary}
+                                        type="number"
+                                        className="grow"
+                                        {...register("salary", {
+                                          required: true,
+                                        })}
+                                      />
+                                    </label>
+                                    {errors.salary && (
+                                      <span className="text-red-600 text-sm">
+                                        Salary is required
+                                      </span>
+                                    )}
+                                  </div>
 
-                                <div className="w-full md:w-1/3">
-                                  <select
-                                    className="input input-ghost w-full"
-                                    {...register("month", { required: true })}
+                                  <div className="w-full md:w-1/3">
+                                    <select
+                                      className="input input-ghost w-full"
+                                      {...register("month", { required: true })}
+                                    >
+                                      <option>Select a month</option>
+                                      <option value="january">January</option>
+                                      <option value="february">February</option>
+                                      <option value="march">March</option>
+                                      <option value="april">April</option>
+                                      <option value="may">May</option>
+                                      <option value="june">June</option>
+                                      <option value="july">July</option>
+                                      <option value="august">August</option>
+                                      <option value="september">September</option>
+                                      <option value="october">October</option>
+                                      <option value="november">November</option>
+                                      <option value="december">December</option>
+                                    </select>
+                                    {errors.month && (
+                                      <span className="text-red-600 text-sm">
+                                        Month is required
+                                      </span>
+                                    )}
+                                  </div>
+
+                                  <div className="w-full md:w-1/3">
+                                    <label className="input input-bordered flex items-center">
+                                      <input
+                                        type="number"
+                                        className="bg-white w-full"
+                                        placeholder="Year"
+                                        {...register("year")}
+                                      />
+                                    </label>
+                                  </div>
+                                </div>
+                                <div className="flex justify-center mt-4">
+                                  <button
+                                    type="submit"
+                                    className="btn btn-sm bg-[#6F42C1] text-white"
                                   >
-                                    <option>Select a month</option>
-                                    <option value="january">January</option>
-                                    <option value="february">February</option>
-                                    <option value="march">March</option>
-                                    <option value="april">April</option>
-                                    <option value="may">May</option>
-                                    <option value="june">June</option>
-                                    <option value="july">July</option>
-                                    <option value="august">August</option>
-                                    <option value="september">September</option>
-                                    <option value="october">October</option>
-                                    <option value="november">November</option>
-                                    <option value="december">December</option>
-                                  </select>
-                                  {errors.month && (
-                                    <span className="text-red-600 text-sm">
-                                      Month is required
-                                    </span>
-                                  )}
+                                    Pay Now
+                                  </button>
                                 </div>
-
-                                <div className="w-full md:w-1/3">
-                                  <label className="input input-bordered flex items-center">
-                                    <input
-                                      type="number"
-                                      className="bg-white w-full"
-                                      placeholder="Year"
-                                      {...register("year")}
-                                    />
-                                  </label>
-                                </div>
-                              </div>
-                              <div className="flex justify-center mt-4">
-                                <button
-                                  type="submit"
-                                  className="btn btn-sm bg-[#6F42C1] text-white"
-                                >
-                                  Pay Now
-                                </button>
-                              </div>
-                            </form>
-                            <div className="modal-action">
-                              <form method="dialog">
-                                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-                                  <RxCross2 />
-                                </button>
                               </form>
+                              <div className="modal-action">
+                                <form method="dialog">
+                                  <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                                    <RxCross2 />
+                                  </button>
+                                </form>
+                              </div>
                             </div>
-                          </div>
-                        </dialog>
-                      </>
-                    ) : (
-                      <button disabled className="cursor-not-allowed">
-                        Pay
-                      </button>
-                    )}
-                  </span>
-                </td>
-                <td>
-                  <span className="px-3 py-1 font-semibold rounded-md bg-[#6F42C1] text-white">
-                    <Link to={`details/${item?.email}`}>
-                      <span>Details</span>
-                    </Link>
-                  </span>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+                          </dialog>
+                        </>
+                      ) : (
+                        <button disabled className="cursor-not-allowed">
+                          Pay
+                        </button>
+                      )}
+                    </span>
+                  </td>
+                  <td>
+                    <span className="px-3 py-1 font-semibold rounded-md bg-[#6F42C1] text-white">
+                      <Link to={`details/${item?.email}`}>
+                        <span>Details</span>
+                      </Link>
+                    </span>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+
+      {/* Grid View (Always for Tablet & Mobile, optionally for Desktop) */}
+      <div
+        className={`grid gap-4 grid-cols-1 lg:grid-cols-3 ${
+          isTableView ? "lg:hidden" : "lg:grid"
+        }`}
+      >
+        {userInfo?.map((item, idx) => (
+          <div key={idx} className="bg-white shadow-md rounded-lg p-4">
+            <h3 className="font-bold text-lg">{item?.name ? item?.name : "N/A"}</h3>
+            <p>Email: {item?.email ? item?.email : "N/A"}</p>
+            <p>Bank A/C No: {item?.bank_account_no ? item?.bank_account_no : "N/A"}</p>
+            <p>Salary: {item?.salary ? item?.salary : "N/A"}</p>
+            <p>
+              Verify Status:{" "}
+              {item.isVerified ? (
+                <BsCheckSquareFill className="text-green-500 inline" />
+              ) : (
+                <ImCross
+                  onClick={() => {
+                    handleVerification(item?._id);
+                  }}
+                  className="text-red-600 cursor-pointer inline"
+                />
+              )}
+            </p>
+            <div className="flex gap-2 mt-2">
+              <button
+                onClick={() => {
+                  if (item.isVerified) {
+                    setSelectedUserid(item._id);
+                    document.getElementById("item?._id").showModal();
+                  }
+                }}
+                className={`btn btn-sm bg-[#6F42C1] text-white ${
+                  item.isVerified ? "cursor-pointer" : "cursor-not-allowed"
+                }`}
+              >
+                Pay
+              </button>
+              <Link to={`details/${item?.email}`} className="btn btn-sm bg-violet-400 text-gray-900">
+                Details
+              </Link>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
